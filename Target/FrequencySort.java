5112618a9f5f029968cc184bea4494605834f047
Kushal Singh <innocent_kushal@Kushals-MacBook-Pro.local>
2026-05-05 15:45:52 -0400

    sort by frequency using bucket sort

--- Target/FrequencySort.java
+++ Target/FrequencySort.java
@@ -1,11 +1,14 @@
 package Target;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
 public class FrequencySort {
     
+    //using priority queue
     public String sortCharacterByFrequency(String s){
         //aabbccc, //aaAbCC
         Map<Character, Integer> f = new HashMap<>();
@@ -27,5 +30,41 @@ public class FrequencySort {
         }
 
         return sb.toString();
+    }
+
+    //using bucket sort
+
+    public String sortFrequency(String s){
+        //tree, t:1, r:1, e:2 bucket: [1[t,r]][2[e]]
+        Map<Character, Integer> map = new HashMap<>();
+        for(char ch: s.toCharArray()){
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        //{(t,1), (r, 1), (e, 2)}
+        //size is length + 1, considering a string is "aaaa", so 
+        // that makes the highest frequency 4
+        List<Character>[] buckeLists = new List[s.length() + 1]; 
+        for( char ch: map.keySet()){
+            if(buckeLists[map.get(ch)] == null) 
+                buckeLists[map.get(ch)] = new ArrayList<>();
+            buckeLists[map.get(ch)].add(ch);
+        }
+
+        //finally building the string 
+        //1 - t, r
+        //2 - e
+        //3
+        //4
+        StringBuilder finalString = new StringBuilder();
+        for (int i = buckeLists.length -1; i > 0; i--){
+            if(buckeLists[i] != null){
+                for(char ch: buckeLists[i]){
+                    for(int j = 0; j < i; j++){
+                        finalString.append(ch);
+                    }
+                }
+            }
+        }
+        return finalString.toString();
     }
 }
